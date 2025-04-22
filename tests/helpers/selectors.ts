570fcdaa229ba0a94461e2d7b85a0670b2bcdaea
Eunji Song <eunjisong@Eunjis-MBP.lan>
2025-04-22 00:12:04 -0400

    typo

--- tests/helpers/selectors.ts
+++ tests/helpers/selectors.ts
@@ -1,4 +1,4 @@
-// iOS와 Android 모두에서 텍스트로 요소를 선택할 수 있도록 도와주는 함수.
+// 텍스트로 요소 선택
 function byText(text: string) {
   const isIOS = driver.isIOS
   return isIOS
@@ -6,7 +6,7 @@ function byText(text: string) {
     : $(`//*[@text="${text}"]`)
 }
 
-// iOS와 Android 모두에서 testID로 요소를 선택할 수 있도록 도와주는 함수.
+// testID로 요소 선택
 function byId(testID: string) {
     // 해보세요
 }
